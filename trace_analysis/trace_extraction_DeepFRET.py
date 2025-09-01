32d132055de115bb87e63a51e34a17f34eed1bd8
Qing-Zhao <qing.zhao@physics.ox.ac.uk>
2025-09-01 19:37:15 +0100

    remove negative trace filtering

--- trace_analysis/trace_extraction_DeepFRET.py
+++ trace_analysis/trace_extraction_DeepFRET.py
@@ -22,8 +22,8 @@ def create_circular_masks(image_shape, yx, inner_radius, gap_width, outer_radius
     return center_mask, bg_mask
 
 
-def calculate_intensities(movie_path, hdf5_path, inner_radius=2, gap_width=1,
-                          roi=(0, 428, 684, 856), outer_radius=5, raw=False):
+def calculate_intensities(movie_path, hdf5_path, inner_radius=1.5, gap_width=1,
+                          roi=(0, 428, 684, 856), outer_radius=2.5, raw=False):
     """
     adapted from DeepFRET
     raw:
@@ -31,7 +31,10 @@ def calculate_intensities(movie_path, hdf5_path, inner_radius=2, gap_width=1,
         return signal-background and background as zeroes.
     """
     locs = pd.read_hdf(hdf5_path, key='locs')
+    locs = locs[locs['frame'] == 0]
     yx_coords = locs[['y', 'x']].to_numpy()
+    #groups = np.unique(locs['group'])
+    #yx_coords = locs.groupby(by='group').mean()[['y', 'x']].to_numpy()
 
     image_stack = imread(movie_path)
     image_stack = image_stack[:, roi[0]:roi[2], roi[1]:roi[3]]
@@ -53,14 +56,13 @@ def calculate_intensities(movie_path, hdf5_path, inner_radius=2, gap_width=1,
         )
 
         # Calculate intensities
-        # Calculate intensities
         roi_pixels = image_stack[:, center_mask]
-        roi_sum = np.sum(roi_pixels, axis=1)
+        roi_sum = np.median(roi_pixels, axis=1)#np.sum(roi_pixels, axis=1)
 
         bg_values = image_stack[:, bg_mask]
         bg_median = np.median(bg_values, axis=1)
-        n_roi_pixels = np.sum(center_mask)
-        bg_sum = bg_median * n_roi_pixels
+        #n_roi_pixels = np.sum(center_mask)
+        bg_sum = bg_median #* n_roi_pixels
 
         if raw:
             signals[:, i] = roi_sum
@@ -70,20 +72,20 @@ def calculate_intensities(movie_path, hdf5_path, inner_radius=2, gap_width=1,
             backgrounds[:, i] = 0
 
 
-    signal_df = pd.DataFrame(signals,  columns=locs.index)
+    signal_df = pd.DataFrame(signals,  columns=locs.index) # columns=locs.index
     # Filter 1: Remove signals with too many negative values
-    negative_frac = (signal_df < 0).mean(axis=0)
-    signal_df_filtered = signal_df.loc[:, negative_frac < 0.3]
+    # negative_frac = (signal_df < 0).mean(axis=0)
+    # signal_df_filtered = signal_df.loc[:, negative_frac < 0.3]
+    #
+    # # Filter 2: Remove constant signals (low standard deviation)
+    signal_stds = signal_df.std(axis=0)
+    signal_df_filtered = signal_df.loc[:, signal_stds >= 1]
 
-    # Filter 2: Remove constant signals (low standard deviation)
-    signal_stds = signal_df_filtered.std(axis=0)
-    signal_df_filtered = signal_df_filtered.loc[:, signal_stds >= 10]
-
-    # Save the filtered results
+    # # Save the filtered results
     save_path = movie_path.replace('.tif', '_DeepFRET_intensity.csv')
     signal_df_filtered.to_csv(save_path, index=False)
-
-    # Also filter the background array to match
+    #
+    # # Also filter the background array to match
     valid_columns = signal_df_filtered.columns
     backgrounds_filtered = backgrounds[:, [i for i, col in enumerate(locs.index) if col in valid_columns]]
 
@@ -91,5 +93,5 @@ def calculate_intensities(movie_path, hdf5_path, inner_radius=2, gap_width=1,
 
 
 if __name__ == '__main__':
-    calculate_intensities(movie_path="J:/non_competitive/20250319_8nt_NComp_GAP_G_Seal100nM/8nt_NComp_GAP_G_Seal100nM_GAP_G_seal3G_100nM_corrected.tif",
-                          hdf5_path="J:/non_competitive/20250319_8nt_NComp_GAP_G_Seal100nM/8nt_NComp_GAP_G_Seal100nM_GAP_G_localization_corrected_boxsize5.hdf5")
+    calculate_intensities(movie_path="G:/CAP_dwellTime_analysis/confidence_0.5/threshold_2median/20250713_CAP_library_1baseNNN/sequencing/CAP_library_1baseNNN_seq_5G_corrected.tif",
+                          hdf5_path="G:/CAP_dwellTime_analysis/confidence_0.5/threshold_2median/20250713_CAP_library_1baseNNN/sequencing/localization2_corrected_picasso_bboxes.hdf5")
